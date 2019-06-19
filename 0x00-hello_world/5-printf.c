a8c497e52c10301e40e73b8557b1c0c14f1044af
Alafresh <868@holbertonschool.com>
2019-06-19 19:12:32 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - display a string
+*
+* Return: success exit
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
