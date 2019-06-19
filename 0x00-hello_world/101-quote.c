ac5d37b77f55b8bf9ba75de46858ee7c9db99f55
Alafresh <868@holbertonschool.com>
2019-06-19 21:51:48 +0000

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - print a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+}
