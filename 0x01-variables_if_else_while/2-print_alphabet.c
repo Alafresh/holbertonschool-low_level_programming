eaad2df77d86d189751887455a8ce9c29098ef85
Alafresh <868@holbertonschool.com>
2019-06-20 19:26:16 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the last number is greater than 5,less than 6 or zero
+ *
+ * Return: take an array from char
+ */
+int main(void)
+{
+char a;
+a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
