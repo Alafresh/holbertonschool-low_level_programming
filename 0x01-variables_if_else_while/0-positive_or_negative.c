8eba9264d8e057031425c4990879f1efb8b564b0
Alafresh <868@holbertonschool.com>
2019-06-20 18:12:26 +0000

    the last hope

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n > 0)
+if (n == 0)
 {
-printf("%d is positive\n", n);
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
@@ -26,7 +26,7 @@ printf("%d is negative\n", n);
 }
 else
 {
-printf("is zero\n");
+printf("%d is positive\n", n);
 }
 return (0);
 }
