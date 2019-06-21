426c260cf1d742ffec83a5bb9883b41c9c024797
Alafresh <868@holbertonschool.com>
2019-06-21 04:57:55 +0000

    ha!

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - prints the numbers from 00 to 99
+*
+* Return: take an array from char
+*/
+int main(void)
+{
+int a;
+int b;
+for (a = '0'; a <= '8'; a++)
+{
+for (b = '1'; b <= '9'; b++)
+{
+if (a < b)
+{ 
+if (a != b)
+{
+putchar(a);
+putchar(b);
+if (a <= '7' || b <= '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
