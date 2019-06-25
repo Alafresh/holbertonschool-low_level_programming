12c84e6aa09c7fae45bee4200b0da7da56598790
Alafresh <868@holbertonschool.com>
2019-06-25 15:16:30 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+* print_sign - prints the sign of a number.
+* @n: is the variable to compar.
+* Return: 1, -1 or 0 depends of the variable.
+*/
+
 int print_sign(int n)
 {
-
 	if (n == 0)
 	{
-		_putchar("Is greater than zero");
+		_putchar('0');
+		return (0);
 	}
-
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
