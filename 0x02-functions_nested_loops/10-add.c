e0371ed99fca9c81fe7b36939bea90cddf3eb0c6
Alafresh <868@holbertonschool.com>
2019-06-25 22:58:51 +0000

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add  - check the code for Holberton School students.
+* @a: the value to sum
+* @b: the value to sum
+* Return: Always 0.
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
