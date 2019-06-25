e768e8909e8d29d30201a32b883b613bc06fe61d
Alafresh <868@holbertonschool.com>
2019-06-25 19:48:23 +0000

    3 colors, 10 digits and 7 notes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* print_last_digit - print the last digit of a number..
+* @a: is a value that we going to evaluate.
+* Return: Always 0.
+*/
+
+int print_last_digit(int a)
+{
+	int b;
+
+	b = a % 10;
+	if (b < 0)
+	{
+		b = -1 * b;
+		_putchar(b + '0');
+	}
+	else
+	{
+		_putchar(b + '0');
+	}
+	return (b);
+	}
