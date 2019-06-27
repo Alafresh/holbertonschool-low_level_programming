bafc6d50cbf74f7310f0dc0d11517e3436675262
Alafresh <868@holbertonschool.com>
2019-06-27 23:17:36 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - insteainstead multiplies of 3 print Fizz and 5 print Buzzz.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int a;
+
+	a = 1;
+	do {
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			a++;
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			a++;
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			a++;
+			printf("Fizz ");
+		}
+		if (a == 101)
+		{
+			break;
+		}
+		printf("%d ", a);
+		a++;
+	}	while (a <= 99);
+	printf("\n");
+	return (0);
+}
