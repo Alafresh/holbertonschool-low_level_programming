ffaab932d60654430332b94a418b62ef37c96ffc
Alafresh <868@holbertonschool.com>
2019-06-27 18:28:17 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _isupper - check the variable is UPPERCASE.
+* @c: is the variable.
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	if (c == 'a')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
