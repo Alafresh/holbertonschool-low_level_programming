53ca5a26cecb962f12450554a15c76215509db1b
Alafresh <868@holbertonschool.com>
2019-07-08 22:56:25 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds the first occurrence of th substring.
+ * @haystack: main string to be scanned
+ * @needle: small string to be searched
+ * Return: a pointer to the first occurrence.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			return (haystack);
+		}
+	haystack++;
+	}
+return (haystack);
+}
