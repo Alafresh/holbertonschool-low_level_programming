a4c1459e62693d317dc1cf3d59afe9c19478a024
Alafresh <868@holbertonschool.com>
2019-07-08 22:44:14 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s:string to be scanned
+ * @c: character to be searched
+ * Return: a pointer to the first occurrence of the character c in string.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	{
+		if (s++)
+		{
+			return (s);
+		}
+	}
+return (0);
+}
