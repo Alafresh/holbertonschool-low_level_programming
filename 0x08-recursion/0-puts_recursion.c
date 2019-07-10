30df62c15940f6e34498bb566399b5ff30b82d49
Alafresh <868@holbertonschool.com>
2019-07-10 18:18:13 +0000

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ * @s: pointer.
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+	_puts_recursion(s);
+	if (*s == '\0')
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This project is about recursion (iterative programming) is the repetition of a block of statements within a computer program (using a loop) 
