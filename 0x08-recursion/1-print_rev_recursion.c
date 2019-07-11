e7ed95fe706cf3102d73331bb4708e1fd70c8796
Alafresh <868@holbertonschool.com>
2019-07-11 04:54:45 +0000

    rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int b;
-
 	if (*s != '\0')
-		_print_rev_recursion(s + 1);
-	b = '\0';
-	if (s[b] >= '\0')
 	{
-		b--;
-		_putchar(s[b]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
