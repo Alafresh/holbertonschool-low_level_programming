eb1339d4ed6d6fdc18206c76cc704c3395d2623b
Alafresh <868@holbertonschool.com>
2019-07-14 20:36:44 +0000

    to infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: 0 or 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc >= 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= 48 && *argv[i] <= 57)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	printf("%d\n", sum);
+	}
+	else
+	return (0);
+}
