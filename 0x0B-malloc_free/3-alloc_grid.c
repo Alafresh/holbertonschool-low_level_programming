0fc2b8576f79620dd775bcb6ff7223f27d3315cd
Alafresh <868@holbertonschool.com>
2019-07-16 23:58:18 +0000

    hey

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,14 +18,14 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	ptr = malloc(width * sizeof(int *));
+	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(height * sizeof(int));
+		ptr[i] = malloc(width * sizeof(int));
 			if (ptr[i] == NULL)
 			{
 				for (l = i; l >= 0; l--)
