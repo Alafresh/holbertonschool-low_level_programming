acd25d4b6a3d7f4d8930051a17d28c59049c00b0
Alafresh <868@holbertonschool.com>
2019-07-16 21:43:53 +0000

    who is not courageous enough to take risks!

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,8 +29,10 @@ char *str_concat(char *s1, char *s2)
 	char *concat, *phrase;
 	int i;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = _length(s1) + _length(s2);
 	phrase = malloc(i * sizeof(char) + 2);
 
