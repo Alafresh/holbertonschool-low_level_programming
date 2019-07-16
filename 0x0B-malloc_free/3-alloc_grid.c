b1410c0e158ff220a5c842880c1e420428ffd11e
Alafresh <868@holbertonschool.com>
2019-07-16 23:04:24 +0000

    beating me

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - pointer to pointer to a 2 dimensional array.
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to pointer.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr, i, j, count;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ptr = (int **)malloc(width * sizeof(int *));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < width)
+	{
+		ptr[i] = (int *)malloc(height * sizeof(int));
+		i++;
+	}
+	count = 0;
+	while (i < width)
+	{
+		for (j = 0; j < height; j++)
+			ptr[i][j] = ++count;
+	}
+return (ptr);
+}
