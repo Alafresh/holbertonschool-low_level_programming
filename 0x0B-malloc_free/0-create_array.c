429bdc0e2d74470fcf8dc61f32dc0809a8a399f7
Alafresh <868@holbertonschool.com>
2019-07-16 17:29:26 +0000

    float like a butterfly!

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	str = malloc(sizeof(char) * size);
-		while (str[i] < size)
+		while (str[i] == 0)
 		{
 			str[i] = c;
 			i++;
