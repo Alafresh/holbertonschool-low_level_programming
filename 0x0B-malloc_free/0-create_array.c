cd64a4d748b3025851dc154c41bcc57b6311d362
Alafresh <868@holbertonschool.com>
2019-07-16 17:08:10 +0000

    float li a butterfly

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it.
+ * @size: size of array
+ * @c: specific char.
+ * Return: null or pointer.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i = 0;
+
+	str = malloc(sizeof(c) * size);
+	while (str[i] == 0)
+	{
+		str[i] = c;
+		i++;
+	}
+	if (size == 0)
+	return (NULL);
+	return (str);
+}
