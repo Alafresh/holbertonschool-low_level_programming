1240d75ed11e0119bafe3e685a279cbeb02064ec
Alafresh <868@holbertonschool.com>
2019-07-22 19:02:09 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - this dog has name, age, and owner.
+* @name: his name depends.
+* @age: his age is variable
+* @owner: and his owner is lost
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
