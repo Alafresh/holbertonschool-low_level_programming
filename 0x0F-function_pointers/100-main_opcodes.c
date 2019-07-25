95ae9feaca7881c85ce39b23677e4724c99c2994
Alafresh <868@holbertonschool.com>
2019-07-25 03:08:57 +0000

    most hackers are young !

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - print opcodes of its own main function
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 always
+*/
+
+int main(int argc, char *argv[])
+{
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+return (0);
+}
