a2d3351ecb278c194b102adfde2d720047b561d7
Alafresh <868@holbertonschool.com>
2019-08-01 21:26:03 +0000

    function to adda a new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* add_node - adds a new node at the beginning of a list_t list
+* @head: double pointer to linked list
+* @str: string to be duplicated
+* Return: the address of the new element
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+		list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
+			new_node->str = strdup(str);
+			new_node->len = strlen(str);
+			new_node->next = (*head);
+			(*head) = new_node;
+return (new_node);
+}
