5a79537e7a4a83bc118e7e19f842eefa484ce44c
Alafresh <868@holbertonschool.com>
2019-08-01 21:34:05 +0000

    function to adda a new nodegit add 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,8 +16,26 @@ list_t *add_node(list_t **head, const char *str)
 			return (NULL);
 		}
 			new_node->str = strdup(str);
-			new_node->len = strlen(str);
+			new_node->len = _strlen(str);
 			new_node->next = (*head);
 			(*head) = new_node;
 return (new_node);
+}
+
+/**
+* _strlen - prototype function.
+* @s: char pointer.
+* Return: the length of a string.
+*/
+
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+	return (count);
 }
