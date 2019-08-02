3193d592c03fb74a746df3775028f603798fea9e
Alafresh <868@holbertonschool.com>
2019-08-02 00:06:38 +0000

    fix typos

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	list_t *last = (*head);
 
+	if (str == NULL)
+		return (NULL);
 	if (new_node == NULL)
 		return (NULL);
 
