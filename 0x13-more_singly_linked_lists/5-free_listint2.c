53048fb4f68758be0ea3118d7f21477e0fb3719b
Alafresh <868@holbertonschool.com>
2019-08-06 16:23:15 +0000

    why

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,12 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr = *head;
+	listint_t *ptr;
 	listint_t *tmp;
 
 	if (head == NULL)
 		return;
+	ptr = *head;
 	while (ptr != NULL)
 	{
 		tmp = ptr;
