23e32a7613c4bf537104238fa62fac4012b01ce8
Alafresh <868@holbertonschool.com>
2019-08-07 04:56:10 +0000

    laal

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		de = first;
 		first = first->next;
 		de->next = first->next;
-		free(first);
+		free(first->next);
 		return (1);
 	}
 return (-1);
