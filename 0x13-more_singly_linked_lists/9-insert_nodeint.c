a6d4e314bb4b70f9415c238f710437731775fcab
Alafresh <868@holbertonschool.com>
2019-08-07 05:09:32 +0000

    j

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (idx < i)
+		return (NULL);
 	if (head == NULL)
 		return (NULL);
 	first = (*head);
