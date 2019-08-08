695d3fd0eeb6fd25090819cd46f48342af8407a2
Alafresh <868@holbertonschool.com>
2019-08-08 22:58:19 +0000

    add other function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,28 @@
 #include "holberton.h"
 
 /**
-* print_binary - function that prints the binary representation of a number
+* printaux - function that prints the binary representation of a number
+* @n: binary number
+*/
+
+void printaux(unsigned long int n)
+{
+	if (n > 1)
+		printaux(n >> 1);
+	_putchar('0' + (n & 1));
+	if (n == 0)
+		return;
+}
+
+/**
+* print_binary - function that prints the binary representation
 * @n: binary number
 */
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar('0' + (n & 1));
+	if (n >= 1)
+		printaux(n);
+	if (n == 0)
+		_putchar('0');
 }
