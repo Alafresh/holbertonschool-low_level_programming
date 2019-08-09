2b9757717b24275345609619cc65939f15b9c9ef
Alafresh <868@holbertonschool.com>
2019-08-09 02:04:18 +0000

    my brother help me

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* clear_bit - function that sets the value of a bit to 1
+* @n: number
+* @index: index
+* Return: 1 worked or -1 error
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = sizeof(unsigned long int) * 8;
+
+	if (index > mask)
+		return (-1);
+	*n |= 1 << index;
+	*n =  ~((1 << index; &n))
+	return (1);
+}
