ad9edb3c81cc73b1d1c29da58fc6608441661cdf
Alafresh <868@holbertonschool.com>
2019-08-09 02:50:06 +0000

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > mask)
 		return (-1);
-	*n |= 1 << index;
-	*n =  ~((1 << index; &n))
+	*n =  (~(1 << index) & *n);
 	return (1);
 }
