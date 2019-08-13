dbaca3ce27d210bde7ddeee99926175cafae2048
Alafresh <868@holbertonschool.com>
2019-08-13 23:40:54 +0000

    close the file descriptiom

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -35,9 +35,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (-1);
+	}
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	wr = write(fd, text_content, _strlen(text_content));
 	if (wr == -1)
 		return (-1);
