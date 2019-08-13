b6cf2c30bfc56bdd312652457b34902d37fbdbd0
Alafresh <868@holbertonschool.com>
2019-08-13 23:39:05 +0000

    delete && conditional

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,9 +34,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (text_content == NULL && fd != -1)
+	if (text_content == NULL)
 		return (-1);
-	else if (fd == -1)
+	if (fd == -1)
 		return (-1);
 	wr = write(fd, text_content, _strlen(text_content));
 	if (wr == -1)
