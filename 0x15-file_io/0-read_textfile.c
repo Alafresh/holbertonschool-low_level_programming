1ba8c16f5c22dc0cc3ca8543a20efd29fee0a1cb
Alafresh <868@holbertonschool.com>
2019-08-13 20:02:10 +0000

    free malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buf = malloc(sizeof(letters));
 	if (buf == NULL)
+	{
 		return (0);
+		free(buf);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
