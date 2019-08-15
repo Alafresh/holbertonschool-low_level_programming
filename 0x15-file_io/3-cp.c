63be3d400f465144afb68d87933a2db4a45e5f98
Alafresh <868@holbertonschool.com>
2019-08-15 20:48:20 +0000

    try to do it

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,32 +1,52 @@
 #include "holberton.h"
 /**
+* err97 - exit with code 97
+* @a: is the argc
+*/
+
+void err97(int a)
+{
+	if (a != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+* err98 - exit with code 98
+* @ptr: pointer
+*/
+
+void err98(char *ptr)
+{
+	if (ptr == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ptr);
+;
+		exit(98);
+	}
+}
+
+/**
 * main - program that copies the content of a file to another file
 * @argc: argument count
 * @argv: argument vector
 * Return: the copies content of a file
 */
-
 int main(int argc, char *argv[])
 {
 	int inputFd, outputFd, cloIn, cloOu;
 	ssize_t numRead;
 	char buf[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		exit(97);
-	}
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	err97(argc);
+	err98(argv[1]);
 	inputFd = open(argv[1], O_RDONLY);
 	if (inputFd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-		exit(99);
+		exit(98);
 	}
 	outputFd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (outputFd == -1)
