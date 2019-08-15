bfdd0ad719981dd05a8c8147f2c79af8f65a8c08
Alafresh <868@holbertonschool.com>
2019-08-15 20:29:01 +0000

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-	int inputFd, outputFd;
+	int inputFd, outputFd, cloIn, cloOu;
 	ssize_t numRead;
 	char buf[1024];
 
@@ -19,25 +19,39 @@ int main(int argc, char *argv[])
 	}
 	if (argv[1] == NULL)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	inputFd = open(argv[1], O_RDONLY);
 	if (inputFd == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 	}
 	outputFd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (outputFd == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	numRead = read(inputFd, buf, 1024);
 	if (numRead == -1)
 	{
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		exit(98);
 	}
 	write(outputFd, buf, numRead);
-	close(inputFd);
-	close(outputFd);
+	cloIn = close(inputFd);
+	if (cloIn == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cloIn);
+		exit(100);
+	}
+	cloOu = close(outputFd);
+	if (cloOu == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cloOu);
+		exit(100);
+	}
 return (0);
 }
