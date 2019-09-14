35e08535c969f50cf8d0c1cc4bd66911c0857225
Alafresh <868@holbertonschool.com>
2019-09-14 19:02:41 +0000

    list length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+* dlistint_len - function that returns number of elements
+* @h: doubly linked list
+* Return: numbe of elements in a linked list
+*/
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
