c80086da2dfa0fac30ccd5a1b6b083bdaf62c99c
Alafresh <868@holbertonschool.com>
2019-09-14 19:00:03 +0000

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+* print_dlistint - prints all the elements of a dlistint_t
+* @h: doubly linked list
+* Return: number of elements
+*/
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
