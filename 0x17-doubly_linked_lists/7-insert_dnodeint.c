bbbac3f7b0d0bc377775ee3d6f320082e34dbff4
Alafresh <868@holbertonschool.com>
2019-09-15 15:48:30 +0000

    insert at node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+* dlistint_len - function that returns number of elements
+* @h: doubly linked list
+* Return: numbe of elements in a linked list
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+return (count);
+}
+
+/**
 * insert_dnodeint_at_index - inserts a new node at given position
 * @h: doubly linked list
 * @idx: index of the list
@@ -10,11 +28,15 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int length;
 	dlistint_t *current = (*h);
 	unsigned int count = 0;
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
+		return (NULL);
+	length = dlistint_len(current);
+	if (length < idx - 1)
 		return (NULL);
 	while (count < idx - 1)
 	{
