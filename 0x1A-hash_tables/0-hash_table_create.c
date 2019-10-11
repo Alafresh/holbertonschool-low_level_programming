1ba2789f3b2770eabdd95bb2b32ae0d2e2bd9d8e
Alafresh <868@holbertonschool.com>
2019-10-11 14:19:08 +0000

    remo one conditional

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	if (size < 1)
-		return (NULL);
 	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
