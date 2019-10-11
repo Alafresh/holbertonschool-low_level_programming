4e9a6e590e318dc8b973af97110c2302e8ca6f5e
Alafresh <868@holbertonschool.com>
2019-10-11 15:06:55 +0000

    % size of the hash table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+* key_index - function that gives you the index of a key
+* @key: is the key
+* @size: size of the array
+* Return: the index at twhich the key
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int i = 0;
+	unsigned long int index = 0;
+
+	while (key && key[i])
+	{
+		index = key[i] % size;
+		i++;
+	}
+	return (index);
+}
