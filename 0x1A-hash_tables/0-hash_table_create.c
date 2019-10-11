63950b9c568b6da23d5ab7fddb0ff8fedf7447b5
Alafresh <868@holbertonschool.com>
2019-10-11 14:14:02 +0000

    delete pointer to struct!

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size < 1)
 		return (NULL);
-	ht = (hash_table_t *)malloc(sizeof(hash_table_t ));
+	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->array = malloc(sizeof(hash_node_t) * size);
