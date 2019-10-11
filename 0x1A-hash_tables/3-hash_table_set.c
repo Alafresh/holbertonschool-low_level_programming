7342055e41767628fdbc4164c851de9a06310378
Alafresh <868@holbertonschool.com>
2019-10-11 21:25:29 +0000

    hey

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,14 +10,14 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *current;
-	hash_node_t *item;
-	unsigned long int index;
+	hash_node_t *current, *item;
+	int index, flag;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	current = ht->array[index];
+	if (!current)
 	{
 		item = malloc(sizeof(hash_node_t));
 		if (item == NULL)
@@ -29,24 +29,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		current = ht->array[index];
 		while (current != NULL)
 		{
-			if (!strcmp(ht->array[index]->key, key))
+			if (strcmp(current->key, key) == 0)
 			{
-				free(ht->array[index]->value);
-				ht->array[index]->value = strdup(value);
-				return (1);
+				current->value = strdup(value);
+				flag = 1;
 			}
 			current = current->next;
 		}
-		item = malloc(sizeof(hash_node_t));
-		if (item == NULL)
-			return (0);
-		item->key = strdup(key);
-		item->value = strdup(value);
-		item->next = ht->array[index];
-		ht->array[index] = item;
+		if (flag == 0)
+		{
+			item = malloc(sizeof(hash_node_t));
+			if (item == NULL)
+				return (0);
+			item->key = strdup(key);
+			item->value = strdup(value);
+			item->next = ht->array[index];
+			ht->array[index] = item;
+		}
 	}
 	return (1);
 }
