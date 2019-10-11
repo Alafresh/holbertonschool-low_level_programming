83c2f4a11774aa3ce0de89e35d14beb9c8afe358
Alafresh <868@holbertonschool.com>
2019-10-11 14:21:29 +0000

    remove cast

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = (hash_table_t)malloc(sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->array = malloc(sizeof(hash_node_t) * size);
