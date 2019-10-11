1aa5501bfb4f0485db276460477454367947e79e
Alafresh <868@holbertonschool.com>
2019-10-11 20:33:36 +0000

    again

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item;
 	int index;
 
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
