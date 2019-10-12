f13771adc5f595e94d1ac0f4a3d0dd0033b57383
root <868@holbertonschool.com>
2019-10-12 22:22:45 +0000

    reuploading second point

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,11 +9,9 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned int hash = hash_djb2(key);
+	unsigned long int hash = hash_djb2(key);
 
 	if (key == NULL)
-		return (0);
-	if (size <= 0)
 		return (0);
 	return (hash % size);
 }
