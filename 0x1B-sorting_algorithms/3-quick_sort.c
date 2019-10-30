67fd25f6ff8f7cf83115030d19789e2522c486b2
Alafresh <868@holbertonschool.com>
2019-10-30 19:35:04 +0000

    quick sort ultimate

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,34 +1,65 @@
 #include "sort.h"
 
-int lomuto(int *array, int low, int high)
+/**
+* lomuto - method implement partition scheme
+* @array: array
+* @low: is 0
+* @high: the size of array - 1
+* @size: size of array
+* Return: the partition of array
+*/
+
+int lomuto(int *array, int low, int high, size_t size)
 {
 	int pivot, i, j;
+
 	i = low;
 	pivot = array[high];
-	for (j = low; j < high - 1; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
-			swap_int(&array[i], &array[j]);
-			print_array(array, high + 1);
+			if (i != j)
+			{
+				swap_int(&array[i], &array[j]);
+				print_array(array, size);
+			}
 			i++;
 		}
 	}
-	swap_int(&array[i], &array[high]);
-		print_array(array, high + 1);
-	return i;
+	if (array[high] < array[i])
+	{
+		swap_int(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
 }
 
-void quickSort(int *array, int l, int h)
+/**
+* quickSort - function improved
+* @array: array
+* @l: low
+* @h: high
+* @size: size of array
+*/
+
+void quickSort(int *array, int l, int h, size_t size)
 {
 	int pi;
+
 	if (l < h)
 	{
-		pi = lomuto(array, l, h);
-		quickSort(array, l, pi - 1);
-		quickSort(array, pi + 1, h);
+		pi = lomuto(array, l, h, size);
+		quickSort(array, l, pi - 1, size);
+		quickSort(array, pi + 1, h, size);
 	}
 }
+
+/**
+* quick_sort - sorts an array of integers
+* @array: array
+* @size: size of array
+*/
 
 void quick_sort(int *array, size_t size)
 {
@@ -36,5 +67,5 @@ void quick_sort(int *array, size_t size)
 
 	high = size - 1;
 	low = 0;
-	quickSort(array, low, high);
+	quickSort(array, low, high, size);
 }
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -25,11 +25,11 @@ void swap_int(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void _swap(listint_t *left, listint_t *right);
 void print_list(const listint_t *list);
-int lomuto(int *array, int low, int high);
+int lomuto(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 size_t dlistint_len(const listint_t *h);
 listint_t *get_dnodeint_at_index(listint_t *head, unsigned int index);
 void selection_sort(int *array, size_t size);
-void quickSort(int *array, int l, int h);
+void quickSort(int *array, int l, int h, size_t size);
 
 #endif
