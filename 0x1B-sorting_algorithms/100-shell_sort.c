80c44aa6ad13bc36ef35d6e161789328498fe53f
Alafresh <868@holbertonschool.com>
2019-10-30 20:37:19 +0000

    add verification when the array is empty

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -10,6 +10,8 @@ void shell_sort(int *array, size_t size)
 	unsigned int knuth, i, j;
 	int arrays;
 
+	if (array == NULL)
+		return;
 	knuth = 1;
 	while (knuth < size / 3)
 		knuth = knuth * 3 + 1;
