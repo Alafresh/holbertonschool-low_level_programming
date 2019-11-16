00132220c03d84ff78de496a0f970cc30919c807
Alafresh <868@holbertonschool.com>
2019-11-15 19:07:57 -0500

    lofi hop

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Comment"""
+
+
 def island_perimeter(grid):
+    """Return the perimeter"""
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] is 1:
-                if grid[i][j + 1] is 0 or j + 1 > len(grid[i]) - 1:
+                if j + 1 > len(grid[i]) - 1 or grid[i][j + 1] is 0:
                     per = per + 1
-                if grid [i][j - 1] is 0 or j - 1 < 0:
+                if j - 1 < 0 or grid[i][j - 1] is 0:
                     per = per + 1
-                if grid[i + 1][j] is 0 or i + 1 > len(grid) - 1:
+                if i + 1 > len(grid) - 1 or grid[i + 1][j] is 0:
                     per = per + 1
-                if grid[i - 1][j] is 0 or i - 1 < 0:
+                if i - 1 < 0 or grid[i - 1][j] is 0:
                     per = per + 1
     return per
