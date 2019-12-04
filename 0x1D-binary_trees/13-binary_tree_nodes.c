dfe9157bfac4d79c1543a356066ed28b490eea31
Alafresh <868@holbertonschool.com>
2019-12-04 15:59:17 -0500

    Nodes

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,26 +1,20 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - Stores recursively
+ * each level in an array of strings
  *
  * @tree: Pointer to the node
- *
- * Return: Number of child nodes
+  *
+ * Return: newNode inserted
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t l;
-	size_t r;
-
-	l = 0;
-	r = 0;
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		l += (binary_tree_nodes(tree->left));
-	if (tree->right)
-		r += (binary_tree_nodes(tree->right));
+
+	if (!tree->left && !tree->right)
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	else
-		return (0);
-	return (l + r + 1);
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
