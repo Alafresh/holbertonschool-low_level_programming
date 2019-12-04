e03ec0e1683020ccf7df4a77287569ee22d82f09
Alafresh <868@holbertonschool.com>
2019-12-04 15:54:47 -0500

    Is perfect? maybe

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -99,7 +99,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_is_full(tree) == 1 && !binary_tree_balance(tree))
+	if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
 	{
 		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		{
