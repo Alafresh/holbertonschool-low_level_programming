1e237142f88f79ef3bd9a6062d9f756a797cb427
Alafresh <868@holbertonschool.com>
2019-12-04 14:23:03 -0500

    Balance

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,11 +1,41 @@
 #include "binary_trees.h"
 #include <stdio.h>
+
 /**
- * binary_tree_postorder - Stores recursively
+ * binary_tree_height - Stores recursively
  * each level in an array of strings
  *
  * @tree: Pointer to the node
- * @func: function pointer
+ *
+ * Return: The branch
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l;
+	size_t r;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		l = 1 + binary_tree_height(tree->left);
+	else
+		l = 0;
+	if (tree->right)
+		r = 1 + binary_tree_height(tree->right);
+	else
+		r = 0;
+	if (l > r)
+		return (l);
+	else
+		return (r);
+}
+
+/**
+ * binary_tree_balance - Stores recursively
+ * each level in an array of strings
+ *
+ * @tree: Pointer to the node
  *
  * Return: newNode inserted
  */
