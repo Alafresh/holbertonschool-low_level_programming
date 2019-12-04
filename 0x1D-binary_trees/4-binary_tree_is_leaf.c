98170ce583d25324c5a5f6fc07e95f308df4b9ac
Alafresh <868@holbertonschool.com>
2019-12-04 12:52:00 -0500

    Is leaf

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -1,17 +1,18 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - Stores recursively
+ * binary_tree_is_leaf - Stores recursively
  * each level in an array of strings
  *
- * @tree: Pointer to the node
+ * @node: Pointer to the node
  *
  * Return: newNode inserted
  */
-int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (tree == NULL)
-		return;
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
-	free(tree);
+	if (node == NULL)
+		return (0);
+	if (!node->left && !node->right)
+		return (1);
+	else
+		return (0);
 }
