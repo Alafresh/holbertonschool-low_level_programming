d481f2a528cb380b0316ffc257a4b56ab2bc2de6
Alafresh <868@holbertonschool.com>
2019-12-04 13:54:32 -0500

    Preorder

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -1,6 +1,19 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_preorder - Stores recursively
+ * each level in an array of strings
+ *
+ * @tree: Pointer to the node
+ * @func: pointer function
+ *
+ * Return: newNode inserted
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || !func)
+		return;
 
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
