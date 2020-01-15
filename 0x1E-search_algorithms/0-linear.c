6155705bae637a39d4eb26e281461094ca558aba
Alafresh <868@holbertonschool.com>
2020-01-15 18:57:00 +0000

    add betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,15 @@
 #include "search_algos.h"
+/**
+* linear_search - searches for a value in an array of integers
+* @array: array
+* @size: size of array
+* @value: value to search
+* Return: the index
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+
 	if (array == NULL)
 		return (-1);
 	while (i < size)
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,21 +1,30 @@
 #include "search_algos.h"
+
+/**
+ * binary_search -  searches for a value in a sorted array
+ * @array: array of integers
+ * @size: the size of array
+ * @value: the value to search
+ * Return: Index or -1 if fails
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
 	int tmp;
 	size_t low, high, mid;
+
 	low = 0;
-	high = size;
+	high = size - 1;
 	if (array == NULL)
 		return (-1);
-	while (low < high)
+	while (low <= high)
 	{
 		mid = (low + high) / 2;
 		tmp = array[mid];
-		print_array(array, high, low);
+/*		printf("This low: %lu, this high: %lu\n", low, high);*/
+		print_array(&array[low], high, low);
 		if (value == tmp)
-		{
 			return (array[mid]);
-		}
 		else if (tmp > value)
 			high = mid - 1;
 		else
@@ -24,12 +33,24 @@ int binary_search(int *array, size_t size, int value)
 	return (-1);
 }
 
+/**
+ * print_array - print array
+ * @high: the index greatest
+ * @low: index lowest
+ * @array: array of integers
+*/
+
 void print_array(int *array, size_t high, size_t low)
 {
-	while (low < high)
+	size_t res, i;
+
+	i = 0;
+	res = high - low;
+	printf("Searching in array: ");
+	while (i < res)
 	{
-		printf("%d, ", array[low]);
-		low++;
+		printf("%d, ", array[i]);
+		i++;
 	}
-	printf("\n");
+	printf("%d\n", array[i]);
 }
